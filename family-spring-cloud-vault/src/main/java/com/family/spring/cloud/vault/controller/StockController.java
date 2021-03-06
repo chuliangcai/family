640ee5151f5b1c9aca1aa1b444291ce8f3650896
chu.yuancheng <cyc18150@126.com>
2021-03-06 17:39:20 +0800

    temp

--- family-spring-cloud-vault/src/main/java/com/family/spring/cloud/vault/controller/StockController.java
+++ family-spring-cloud-vault/src/main/java/com/family/spring/cloud/vault/controller/StockController.java
@@ -3,6 +3,7 @@ package com.family.spring.cloud.vault.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,12 @@ public class StockController {
     @Autowired
     private StockDao stockDao;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/most-actives")
     public List<Stock> findMostActives() {
+        String a = environment.getProperty("spring.main");
         return stockDao.findAll();
     }
 }
