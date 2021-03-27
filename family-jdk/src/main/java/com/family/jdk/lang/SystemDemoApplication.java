a1a03cefecbc3e8c5d412d3afab82011a0523f81
chu.yuancheng <cyc18150@126.com>
2021-03-28 07:38:15 +0800

    s

--- family-jdk/src/main/java/com/family/jdk/lang/SystemDemoApplication.java
+++ family-jdk/src/main/java/com/family/jdk/lang/SystemDemoApplication.java
@@ -3,6 +3,7 @@ package com.family.jdk.lang;
 public class SystemDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println(args);
         System.getenv().forEach((k, v) -> {
             System.out.println(k + "=" + v);
         });
