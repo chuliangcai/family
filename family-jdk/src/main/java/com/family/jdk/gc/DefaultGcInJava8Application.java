f39becd4bde92414e63fc2457499fcdeffbd57ed
chu.yuancheng <cyc18150@126.com>
2021-03-31 12:47:24 +0800

    s

--- family-jdk/src/main/java/com/family/jdk/gc/DefaultGcInJava8Application.java
+++ family-jdk/src/main/java/com/family/jdk/gc/DefaultGcInJava8Application.java
@@ -3,7 +3,9 @@ package com.family.jdk.gc;
 import java.util.concurrent.locks.LockSupport;
 
 public class DefaultGcInJava8Application {
-    public static void main(String[] args) {
-        LockSupport.park();
+    public static void main(String[] args) throws Exception {
+        for (; ; ) {
+            Thread.sleep(1);
+        }
     }
 }
