93f56f173d9ef38021e34d73192659ae1ab4e3f6
chu.yuancheng <cyc18150@126.com>
2021-04-06 09:45:37 +0800

    s

---
+++ family-algorithm/src/main/java/com/family/algorithm/tree/AvlTreeDemo.java
@@ -0,0 +1,123 @@
+package com.family.algorithm.tree;
+
+public class AvlTreeDemo {
+    public static void main(String[] args) {
+        AvlTree avlTree = new AvlTree();
+        avlTree.insert(3);
+        avlTree.insert(2);
+        avlTree.insert(1);
+        System.out.println("ddd");
+    }
+
+    public static class Node {
+        private final int data;
+        private Node left;
+        protected Node right;
+        private int height;
+
+        public Node(int data) {
+            this.data = data;
+            this.height = 1;
+        }
+    }
+
+    public static class AvlTree {
+        private Node root;
+
+        public void insert(int key) {
+            root = insert(root, key);
+        }
+
+        private int height(Node node) {
+            if (node == null) {
+                return 0;
+            }
+            return node.height;
+        }
+
+        /**
+         * 插入
+         *
+         * @param root 局部根节点
+         * @param key  值
+         * @return 新的根节点
+         */
+        private Node insert(Node root, int key) {
+            if (root == null) {
+                return new Node(key);
+            }
+
+            if (root.data == key) {
+                return root;
+            }
+
+            if (key < root.data) {
+                root.left = insert(root.left, key);
+            } else {
+                root.right = insert(root.right, key);
+            }
+
+            if (Math.abs(height(root.left) - height(root.right)) > 1) {
+                root = balance(root);
+            }
+            refreshHeight(root);
+            return root;
+        }
+
+        private void refreshHeight(Node node) {
+            node.height = Math.max(height(node.left), height(node.right)) + 1;
+        }
+
+        private Node balance(Node node) {
+            Node node1, node2;
+            // ll
+            if (height(node.left) > height(node.right) &&
+                    height(node.left.left) > height(node.left.right)) {
+                node1 = node.left;
+                node.left = node1.right;
+                node1.right = node;
+                refreshHeight(node);
+                return node1;
+            }
+            // lr
+            if (height(node.left) > height(node.right) &&
+                    height(node.left.right) > height(node.left.left)) {
+                node1 = node.left;
+                node2 = node.left.right;
+                node.left = node2.right;
+                node1.right = node2.left;
+                node2.left = node1;
+                node2.right = node;
+
+                refreshHeight(node);
+                refreshHeight(node1);
+                return node2;
+            }
+            // rr
+            if (height(node.right) > height(node.left) &&
+                    height(node.right.right) > height(node.right.left)) {
+                node1 = node.right;
+                node.right = node1.left;
+                node1.left = node;
+
+                refreshHeight(node);
+                return node1;
+            }
+            // rl
+            if (height(node.right) > height(node.left) &&
+                    height(node.right.left) > height(node.right.right)) {
+                node1 = node.right;
+                node2 = node.right.left;
+                node.right = node2.left;
+                node1.left = node2.right;
+                node2.left = node;
+                node2.right = node1;
+
+                refreshHeight(node);
+                refreshHeight(node1);
+                return node2;
+            }
+            return node;
+        }
+    }
+}
---
+++ family-algorithm/src/main/java/com/family/algorithm/tree/BinarySearchTreeDemo.java
@@ -0,0 +1,72 @@
+package com.family.algorithm.tree;
+
+public class BinarySearchTreeDemo {
+
+    public static void main(String[] args) {
+        BinarySearchTree bt = new BinarySearchTree();
+        bt.add(8);
+        bt.add(3);
+        bt.add(10);
+        bt.add(1);
+        bt.add(6);
+        bt.add(14);
+        bt.add(4);
+        bt.add(7);
+        bt.add(13);
+        bt.print();
+    }
+
+    public static class BinarySearchTree {
+        private TreeNode root;
+
+        public void add(int data) {
+            if (root == null) {
+                root = new TreeNode(data);
+            } else {
+                root.addNode(data);
+            }
+        }
+
+        public void print() {
+            root.print();
+        }
+    }
+
+    public static class TreeNode {
+        int data;
+        TreeNode leftChild;
+        TreeNode rightChild;
+
+        TreeNode(int data) {
+            this.leftChild = null;
+            this.rightChild = null;
+            this.data = data;
+        }
+
+        public void print() {
+            if (this.leftChild != null) {
+                this.leftChild.print();
+            }
+            System.out.print(this.data + "->");
+            if (this.rightChild != null) {
+                this.rightChild.print();
+            }
+        }
+
+        public void addNode(int data) {
+            if (data < this.data) {
+                if (this.leftChild == null) {
+                    this.leftChild = new TreeNode(data);
+                } else {
+                    this.leftChild.addNode(data);
+                }
+            } else {
+                if (this.rightChild == null) {
+                    this.rightChild = new TreeNode(data);
+                } else {
+                    this.rightChild.addNode(data);
+                }
+            }
+        }
+    }
+}
