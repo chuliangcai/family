93d53f2ab08998a0f31e3f833c70c60e06215124
chu.yuancheng <cyc18150@126.com>
2021-04-07 13:31:39 +0800

    s

---
+++ family-algorithm/src/main/java/com/family/algorithm/offer/Solution55.java
@@ -0,0 +1,39 @@
+package com.family.algorithm.offer;
+
+/**
+ * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+ * https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/submissions/
+ */
+public class Solution55 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    public static int height(TreeNode root) {
+        if (root == null) {
+            //空节点
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            //叶子节点
+            return 1;
+        }
+        return Integer.max(height(root.left), height(root.right));
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(height(root.left) - height(root.right)) <= 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
