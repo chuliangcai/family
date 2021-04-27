2eb01e0fa5460beb547c92eda428fb03587a55b6
chu.yuancheng <cyc18150@126.com>
2021-04-27 12:57:38 +0800

    s

---
+++ family-jdk/src/main/java/com/family/jdk/debug/IdeaDebugDemo.java
@@ -0,0 +1,8 @@
+package com.family.jdk.debug;
+
+public class IdeaDebugDemo {
+    public static void main(String[] args) {
+        String str = "hello word";
+        System.out.println(str);
+    }
+}
---
+++ family-spring-boot/src/main/java/com/family/spring/boot/RemoteDebugDemoApplication.java
@@ -0,0 +1,12 @@
+package com.family.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteDebugDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteDebugDemoApplication.class, args);
+    }
+}
