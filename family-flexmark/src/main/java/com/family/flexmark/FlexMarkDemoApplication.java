f633c0d7225f539ad0ce80df4d2997a6e256fb75
chu.yuancheng <cyc18150@126.com>
2021-04-27 10:46:17 +0800

    s

--- family-flexmark/src/main/java/com/family/flexmark/FlexMarkDemoApplication.java
+++ family-flexmark/src/main/java/com/family/flexmark/FlexMarkDemoApplication.java
@@ -4,20 +4,31 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.vladsch.flexmark.ast.Image;
+import com.vladsch.flexmark.formatter.Formatter;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.util.collection.iteration.ReversiblePeekingIterator;
 import com.vladsch.flexmark.util.data.MutableDataSet;
+import com.vladsch.flexmark.util.sequence.BasedSequence;
 
+/**
+ * 需求：使用flexMark操作markdown，实现一键生产可以粘贴到博客的文章
+ * flexMark地址：
+ */
 public class FlexMarkDemoApplication {
     private static String dirPath = "";
-    public static File destFileDir;
-    private static final String DEST_FILE_PARENT_PATH = "/Users/chuyuancheng/data/projects/freedom";
+    private static File destFileDir;
+    private static String IMAGE_FILE_URL_PREFIX = "https://cdn.jsdelivr.net/gh/chuliangcai/freedom";
+    private static final String DEST_FILE_PARENT_PATH = "/Users/chuyuancheng/data/projects/freedom/";
 
     public static void main(String[] args) throws Exception {
         //要发表的文章路径
@@ -25,6 +36,7 @@ public class FlexMarkDemoApplication {
         MutableDataSet options = new MutableDataSet();
         Parser parser = Parser.builder(options).build();
         String descFile = DEST_FILE_PARENT_PATH + StringUtils.substringAfterLast(file, "blog");
+        IMAGE_FILE_URL_PREFIX = StringUtils.substringBeforeLast(IMAGE_FILE_URL_PREFIX + StringUtils.substringAfterLast(file, "blog"), "/");
         dirPath = StringUtils.substringBeforeLast(file, "/");
         destFileDir = new File(StringUtils.substringBeforeLast(descFile, "/"));
         //noinspection ResultOfMethodCallIgnored
@@ -32,15 +44,40 @@ public class FlexMarkDemoApplication {
         FileUtils.copyFileToDirectory(new File(file), destFileDir);
         Node document = parser.parseReader(new FileReader(file));
         extractNode(document);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println(LocalDateTime.now().format(formatter) + " copy finished");
         Runtime run = Runtime.getRuntime();
-        Process pr = run.exec(new String[]{"/bin/bash", "-c", "pwd;git status;git add -A;git commit -m 's';git push;"});
+        Process pr = run.exec(new String[]{"/bin/bash", "-c", "cd " + DEST_FILE_PARENT_PATH + ";pwd;git status;git add -A;git commit -m 's';git push;"});
         pr.waitFor();
         BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
         String line;
         while ((line = buf.readLine()) != null) {
             System.out.println(line);
         }
-        System.out.println("push finished");
+        System.out.println(LocalDateTime.now().format(formatter) + " push finished");
+        Node replace = parser.parseReader(new FileReader(file));
+        replaceUrl(replace);
+        File outputFile = new File("/Users/chuyuancheng/Desktop/blog_source/" + StringUtils.substringAfterLast(file, "/"));
+        Formatter mFormatter = new Formatter.Builder().build();
+        FileUtils.write(outputFile, mFormatter.render(replace), Charset.defaultCharset());
+        System.out.println(LocalDateTime.now().format(formatter) + " replace url finished");
+    }
+
+    public static void replaceUrl(Node node) throws Exception {
+        if (!node.hasChildren()) {
+            return;
+        }
+        ReversiblePeekingIterator<Node> iterator = node.getChildIterator();
+        while (iterator.hasNext()) {
+            Node node1 = iterator.next();
+            if (node1 instanceof Image) {
+                Image image = (Image) node1;
+                String fileName = image.getUrl().toString();
+                image.setPageRef(BasedSequence.of(IMAGE_FILE_URL_PREFIX + "/" + fileName));
+            } else {
+                replaceUrl(node1);
+            }
+        }
     }
 
     public static void extractNode(Node node) throws Exception {
