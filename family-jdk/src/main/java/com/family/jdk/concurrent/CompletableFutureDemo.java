49b6c8431a629ee4af6d1e766134801a3cf1af16
chu.yuancheng <cyc18150@126.com>
2021-05-29 11:10:23 +0800

    s

--- family-jdk/src/main/java/com/family/jdk/concurrent/CompletableFutureDemo.java
+++ family-jdk/src/main/java/com/family/jdk/concurrent/CompletableFutureDemo.java
@@ -1,4 +1,25 @@
 package com.family.jdk.concurrent;
 
+import java.util.concurrent.CompletableFuture;
+
 public class CompletableFutureDemo {
+    public static void main(String[] args) {
+        CompletableFuture cf = CompletableFuture.runAsync(() -> {
+            System.out.println(Thread.currentThread().isDaemon());
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println("3" + cf.isDone());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        cf.getNow(null);
+        System.out.println("4" + cf.isDone());
+    }
 }
---
+++ family-jdk/src/main/java/com/family/jdk/lang/NewObjDemo.java
@@ -0,0 +1,7 @@
+package com.family.jdk.lang;
+
+public class NewObjDemo {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+    }
+}
