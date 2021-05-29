50ff901a3d89769bca8393512e0c6b7c02007653
chu.yuancheng <cyc18150@126.com>
2021-05-29 18:11:19 +0800

    s

---
+++ family-jdk/src/main/java/com/family/jdk/net/SocketDemo.java
@@ -0,0 +1,10 @@
+package com.family.jdk.net;
+
+import java.net.ServerSocket;
+
+public class SocketDemo {
+    public static void main(String[] args) throws Exception{
+        ServerSocket serverSocket = new ServerSocket(11111);
+//        serverSocke
+    }
+}
