864391a3b1eb2524953c3d5057de1a8a22dd3e6d
chu.yuancheng <cyc18150@126.com>
2021-06-08 21:05:17 +0800

    s

--- family-jdk/src/main/java/com/family/jdk/lang/NewObjDemo.java
+++ family-jdk/src/main/java/com/family/jdk/lang/NewObjDemo.java
@@ -1,7 +1,16 @@
 package com.family.jdk.lang;
 
+import java.util.concurrent.locks.LockSupport;
+
 public class NewObjDemo {
+    public static class Test1{
+
+    }
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
+        Test1 test1=new Test1();
+//        Class.forName()
+        LockSupport.park();
+
     }
 }
--- family-jdk/src/main/java/com/family/jdk/lang/StringInternDemo.java
+++ family-jdk/src/main/java/com/family/jdk/lang/StringInternDemo.java
@@ -2,17 +2,7 @@ package com.family.jdk.lang;
 
 public class StringInternDemo {
     public static void main(String[] args) {
-        //        String s0 = "kvill";
-        //        String s1 = "kvill";
-        //        String s2 = "kv" + "ill";
-        final String str1 = "ja";
-        final String str2 = "va";
-        String str3 = str1 + str2;
-        System.out.println(new String("java").intern() == str3);
-        //        System.out.println(s0 == s1);
-        //        System.out.println(s0 == s2);
-        System.out.println("xxxx");
-        Thread thread=new Thread();
-        Thread.yield();
+       String a = new String("aaa");
+        System.out.println("aa");
     }
 }
