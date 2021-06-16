1e2496aeb8366b06419ad48e9b6ca04099feb83b
chu.yuancheng <cyc18150@126.com>
2021-06-16 18:02:13 +0800

    s

---
+++ family-spring-cloud-provider/src/main/java/com/family/spring/cloud/provider/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.family.spring.cloud.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "hello world";
+    }
+}
